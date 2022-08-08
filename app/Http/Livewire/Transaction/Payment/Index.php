dd06119cd15401f61bff36dc3fe43ced5a292f34
ALI AKBAR <50044603+ALI1111111111@users.noreply.github.com>
2022-08-08 14:48:39 +0500

    configuration done

---
+++ app/Http/Livewire/Transaction/Payment/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Transaction\Payment;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.transaction.payment.index');
+    }
+}
